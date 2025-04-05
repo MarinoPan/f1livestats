a7a17cf3b5ff5bfdf21543f88662dbea4177c8b7
MarinoPan <marinopanariello@gmail.com>
2025-04-05 07:17:17 +0200

    rimozione middleware

--- next.config.ts
+++ next.config.ts
@@ -2,45 +2,13 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"), // Assicura che @ punti a /src
-    };
-    return config;
-  },
-  reactStrictMode: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-  experimental: {
-    optimizeCss: true,
-    serverActions: {
-      bodySizeLimit: "2mb",
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "@": path.resolve(__dirname, "src"), // Assicura che @ punti a /src
+        };
+        return config;
     },
-    serverComponentsExternalPackages: [],
-  },
-  headers: async () => {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
